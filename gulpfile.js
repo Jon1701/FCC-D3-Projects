8676b9ca9a14ab8801ec488439de960193e935cd
Jon Balon <development@jonbalon.com>
2016-08-05 05:05:56 -0400

    Added gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,67 @@
+////////////////////////////////////////////////////////////////////////////////
+// Modules
+////////////////////////////////////////////////////////////////////////////////
+var gulp = require('gulp'); // Gulp.
+var webserver = require('gulp-webserver'); // Gulp webserver.
+var sass = require('gulp-sass'); // SASS.
+var webpack = require('webpack-stream'); // Webpack.
+
+////////////////////////////////////////////////////////////////////////////////
+// Paths
+////////////////////////////////////////////////////////////////////////////////
+var srcPath = './source/';
+var destPath = './build/';
+var modulesPath = './node_modules/';
+
+// Webpack
+gulp.task('webpack', function() {
+  gulp.src(srcPath + 'javascript/index.js')
+    .pipe(webpack({
+      watch: true,
+
+      output: {
+        filename: 'app.js'
+      }
+    }))
+    .pipe(gulp.dest(destPath + 'javascript/'));
+});
+
+// Compile .scss and move.
+gulp.task('stylesheets', function() {
+  gulp.src(srcPath + 'stylesheets/**/*')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest(destPath + 'stylesheets/'));
+});
+
+// Move .html.
+gulp.task('html', function() {
+  gulp.src(srcPath + '*.html')
+    .pipe(gulp.dest(destPath));
+});
+
+////////////////////////////////////////////////////////////////////////////////
+// Webserver
+////////////////////////////////////////////////////////////////////////////////
+gulp.task("webserver", function() {
+  gulp.src(destPath)
+    .pipe(webserver({
+      fallback: "index.html",
+      livereload: true,
+      directoryListing: false,
+      open: false
+    }));
+});
+
+////////////////////////////////////////////////////////////////////////////////
+// Watch task
+////////////////////////////////////////////////////////////////////////////////
+gulp.task('watch', function() {
+  gulp.watch(srcPath + 'stylesheets/**/*.scss', ['stylesheets']); // SASS Main.
+  gulp.watch(srcPath + 'stylesheets/**/_*.scss', ['stylesheets']); // SASS Partials.
+  gulp.watch(srcPath + '*.html', ['html']);
+});
+
+////////////////////////////////////////////////////////////////////////////////
+// Default task
+////////////////////////////////////////////////////////////////////////////////
+gulp.task('default', ['webserver', 'watch', 'stylesheets', 'html', 'webpack']);
