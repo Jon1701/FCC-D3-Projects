9053f14617a2f6746cdcf3bc91bbf7fe59dc8d39
Jon Balon <development@jonbalon.com>
2016-08-05 06:41:14 -0400

    Added scaling

--- source/javascript/BarChart.js
+++ source/javascript/BarChart.js
@@ -22,19 +22,30 @@ var jsonSuccess = function(data) {
   // Access datapoints only.
   var dataset = data['data'];
 
-  console.log(dataset)
-
   // Create bars.
   var bars = svg.selectAll('rect')
                 .data(dataset)
                 .enter()
                 .append('rect');
 
+  // Individual bar width.
+  var barWidth = canvasWidth / dataset.length;
+
+  // Scales the independent variable's values to the canvas width.
+  var xScale = d3.scaleLinear()
+                  .domain([0, dataset.length])
+                  .range([0, canvasWidth]);
+
+  // Scales the dependent variable's values to the canvas height.
+  var yScale = d3.scaleLinear()
+                  .domain([0, d3.max(dataset, function(d) {return d[1]})])
+                  .range([0, canvasHeight]);
+
   // Set individual bar dimensions.
-  bars.attr('x', function(d, i) {return i * canvasWidth/dataset.length})
-      .attr('y', function(d, i) {return canvasHeight - d[1]})
-      .attr('width', function(d, i) {return canvasWidth/dataset.length})
-      .attr('height', function(d, i) {return d[1]})
+  bars.attr('x', function(d, i) {return xScale(i)})
+      .attr('y', function(d, i) {return canvasHeight - yScale(d[1])})
+      .attr('width', function(d, i) {return barWidth - 0.5})
+      .attr('height', function(d, i) {return yScale(d[1])})
 }
 
 // Get GDP data.
