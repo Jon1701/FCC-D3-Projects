0997095a6bda3531e9b0f55c05fd2b3994495eb2
Jon Balon <development@jonbalon.com>
2016-08-05 23:58:16 -0400

    Fixed axes and scales, x-axis now uses a time scale, increased canvas size

--- source/javascript/BarChart.js
+++ source/javascript/BarChart.js
@@ -1,9 +1,40 @@
+var getUniqueYears = function(arr) {
+
+  // Array holding all the extracted years.
+  var yearsArray = [];
+
+  // Get all the years
+  for (var i=0; i<arr.length; i++) {
+
+    // Get the date.
+    var date = arr[i][0];
+
+    // Split into year, month, day.
+    var yearMonthDay = date.split('-');
+
+    // Get just the year.
+    var year = date.split('-')[0];
+
+    // Check to see if the year is already stored into yearsArray.
+    //
+    // If it is not stored, store it.
+    // If it is stored, skip to next element.
+    if (yearsArray.indexOf(year) == -1) {
+      yearsArray.push(year);
+    }
+
+  }
+
+  // Return a unique list of years within the data.
+  return yearsArray;
+}
+
 // Use d3 library.
 var d3 = require('d3');
 
 // Define canvas dimensions.
-var canvasWidth = 1000;
-var canvasHeight = 500;
+var canvasWidth = 1500;
+var canvasHeight = 700;
 
 // Set canvas padding.
 var padding = 60;
@@ -41,9 +72,11 @@ var jsonSuccess = function(data) {
   // Individual bar width.
   var barWidth = canvasWidth / dataset.length;
 
-  // Scales the independent variable's values to the canvas width.
-  var xScale = d3.scaleLinear()
-                  .domain([0, dataset.length])
+  // Scales the
+  var startDate = dataset[0][0]; // Start date.
+  var endDate = dataset[dataset.length-1][0]; // End date.
+  var xScale = d3.scaleTime()
+                  .domain([new Date(startDate), new Date(endDate)])
                   .range([0, graphWidth]);
 
   // Scales the dependent variable's values to the canvas height.
@@ -51,21 +84,20 @@ var jsonSuccess = function(data) {
                   .domain([0, d3.max(dataset, function(d) {return d[1]})])
                   .range([graphHeight, 0]);
 
-  // x-axis.
-  var xAxis = d3.axisBottom(xScale);
+  // Horizontal axis.
+  var xAxis = d3.axisBottom(xScale)
   svg.append('g')
       .attr('transform', translation(padding*leftShift, graphHeight + padding))
       .call(xAxis);
 
-  // y-axis.
+  // Vertical axis.
   var yAxis = d3.axisLeft(yScale);
   svg.append('g')
       .attr('transform', translation(padding*leftShift, padding))
       .call(yAxis);
 
-
   // Set individual bar dimensions.
-  bars.attr('x', function(d, i) {return xScale(i) + padding*leftShift})
+  bars.attr('x', function(d, i) {return xScale(new Date(d[0])) + padding*leftShift})
       .attr('y', function(d, i) {return yScale(d[1]) + padding})
       .attr('width', function(d, i) {return barWidth - 0.5})
       .attr('height', function(d, i) {return graphHeight - yScale(d[1])})
