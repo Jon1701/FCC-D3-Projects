84164fce9ebfcc1c22bbc1fdaa6d8a2077521f76
Jon Balon <development@jonbalon.com>
2016-08-06 20:37:42 -0400

    Added AJAX request code

--- source/javascript/Scatterplot.js
+++ source/javascript/Scatterplot.js
@@ -1,1 +1,30 @@
+// Required libraries.
 var d3 = require('d3');
+
+// Callback to handle AJAX request errors.
+var jsonError = function(error) {
+  console.log(error);
+}
+
+// Callback to handle data retrieved from AJAX request.
+var jsonSuccess = function(data) {
+  console.log(data);
+}
+
+// Load Cyclist Data.
+d3.json('./datasets/cyclist-data.json', function(error, result) {
+
+  // Error checking.
+  if (error) {
+
+    // Error function.
+    jsonError(error);
+
+  } else {
+
+    // If no error occurrs, run the jsonSuccess function.
+    jsonSuccess(result);
+
+  }
+
+});
