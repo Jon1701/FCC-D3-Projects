2b763aa91b5799ebc7da869f3ecfef42f3b077c0
Jon Balon <development@jonbalon.com>
2016-08-06 20:45:08 -0400

    Added canvas properties

--- source/javascript/Scatterplot.js
+++ source/javascript/Scatterplot.js
@@ -1,6 +1,29 @@
 // Required libraries.
 var d3 = require('d3');
 
+// Canvas properties.
+var canvas = {
+  width: 500,
+  height: 500,
+  padding: {
+    vertical: 20,
+    horizontal: 20
+  }
+}
+
+// Graph properties.
+var graph = {
+  width: canvas.width - 2*canvas.padding.horizontal,
+  height: canvas.height - 2*canvas.padding.vertical
+}
+
+// Canvas.
+var svg = d3.select('#canvas')
+            .attr('width', canvas.width)
+            .attr('height', canvas.height);
+
+console.log(svg)
+
 // Callback to handle AJAX request errors.
 var jsonError = function(error) {
   console.log(error);
