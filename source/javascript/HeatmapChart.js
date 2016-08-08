f5be1da61d5bf254537588c3187e61bc06995383
Jon Balon <development@jonbalon.com>
2016-08-08 02:06:14 -0400

    Added correct heatmap colouring, added proper scaling functions.

--- source/javascript/HeatmapChart.js
+++ source/javascript/HeatmapChart.js
@@ -1,14 +1,17 @@
 // Use d3 library.
 var d3 = require('d3');
 
+////////////////////////////////////////////////////////////////////////////////
+// Canvas and graph settings.
+////////////////////////////////////////////////////////////////////////////////
 canvas = {
   dimensions: {
     width: 1920,
-    height: 500
+    height: 600
   },
   padding: {
-    horizontal: 50,
-    vertical: 50
+    horizontal: 100,
+    vertical: 100
   }
 }
 
@@ -19,12 +22,80 @@ graph = {
   }
 }
 
+////////////////////////////////////////////////////////////////////////////////
+// Function used to build an array of integers which will map into an
+// array of colours to be used for the legend
+//
+// domain: Two-dimensional array.
+// scale: Scaling function which maps domain-values to colours.
+//
+// Returns an array of size 2.
+// First array is an array of integers between domain[0] and domain[1].
+// Second array is an array of colours from applying scale() to domain[i].
+////////////////////////////////////////////////////////////////////////////////
+var buildLegendDomainRange = function(domain, scale) {
 
-// Callback to handle data retrieved from AJAX request.
+  // Start and end points.
+  var start = Math.floor(domain[0]);
+  var end = Math.ceil(domain.slice(-1)[0]);
+
+  // For each integers from start to end,
+  // store the integer, and store its mapping from scale().
+  var domainArray = [];
+  var rangeArray = [];
+  for (var i=start; i<=end; i++) {
+    domainArray.push(i);
+    rangeArray.push(scale(i));
+  }
+
+  return [domainArray, rangeArray];
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// Augments the given dataset to include the temperature.
+//
+// For each data point, create a temperature property under each monthly
+// variance with value of base temperature + variance.
+////////////////////////////////////////////////////////////////////////////////
+function augmentData(dataset) {
+
+  // Short hand variables.
+  var baseTemperature = dataset['baseTemperature'];
+  var monthlyVariance = dataset['monthlyVariance'];
+
+  // Go through the monthly variance, add the variance to the base temperature.
+  for (var i=0; i<monthlyVariance.length; i++) {
+
+    // Calculate temperature to be base temperature + monthly variance.
+    var temperature = baseTemperature + monthlyVariance[i]['variance'];
+
+    // Store the temperature rounded to 2 decimal places.
+    monthlyVariance[i]['temperature'] = Math.round(temperature * 100) / 100;
+  }
+
+  // Store updated monthly variance into dataset.
+  dataset['monthlyVariance'] = monthlyVariance;
+
+  // Return modified dataset.
+  return dataset;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// Callback to handle data retrieved from AJAX request
+////////////////////////////////////////////////////////////////////////////////
 var jsonSuccess = function(dataset) {
 
-  // Monthly variance
+  // Augment dataset with temperatures.
+  dataset = augmentData(dataset);
+
+  // Shorthand variable for monthly variance.
   var monthlyVariance = dataset['monthlyVariance'];
+
+  // Calculate temperature range (in integers).
+  var temperatureRange = [
+    d3.min(monthlyVariance, function(d) { return d['temperature']; }),
+    d3.max(monthlyVariance, function(d) { return d['temperature']; })
+  ];
 
   // Create canvas.
   var svg = d3.select('#canvas')
@@ -35,6 +106,12 @@ var jsonSuccess = function(dataset) {
                   .data(monthlyVariance)
                   .enter()
                   .append('rect');
+
+  // Colour scale which maps temperatures to a colour ranging from orange to
+  // dark red.
+  var colourScale = d3.scaleLinear()
+                      .domain(temperatureRange)
+                      .range(['#FFA500', '#8B0000']);
 
   // Horizontal scale. Years to canvas width.
   var xScale = d3.scaleTime()
@@ -52,32 +129,30 @@ var jsonSuccess = function(dataset) {
                   ])
                   .range([0, graph.dimensions.height]);
 
-  cells.attr('x', function(d, i) { return xScale(d['year']) })
-        .attr('y', function(d, i) { return yScale(d['month']) })
+  // Create Heatmap cells.
+  cells.attr('x', function(d, i) { return xScale(d['year']) + canvas.padding.horizontal })
+        .attr('y', function(d, i) { return yScale(d['month']) + canvas.padding.vertical })
         .attr('width', graph.dimensions.width/(monthlyVariance.length/12))
-        .attr('height', graph.dimensions.height/12)
-        .attr('fill', function() {
+        .attr('height', graph.dimensions.height/12 + 3 )
+        .attr('fill', function(d) {
 
-          // Randomly generate colours.
-          var r = Math.floor(Math.random()*255);
-          var g = Math.floor(Math.random()*255);
-          var b = Math.floor(Math.random()*255);
-          var a = Math.random();
-          return 'rgba(' + r + ',' + g + ',' + b + ',' + a + ')';
+          return colourScale(d['temperature']);
 
         });
 
-
+//console.log(buildLegendDomainRange(temperatureRange, colourScale))
 }
 
-
-// Callback to handle AJAX request errors.
+////////////////////////////////////////////////////////////////////////////////
+// Callback to handle AJAX request errors
+////////////////////////////////////////////////////////////////////////////////
 var jsonError = function(error) {
   console.log(error);
 }
 
-
-// Load data.
+////////////////////////////////////////////////////////////////////////////////
+// Load data
+////////////////////////////////////////////////////////////////////////////////
 d3.json('./datasets/global-temperature.json', function(error, result) {
 
   // Error checking.
