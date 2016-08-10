e852209df2998e67940e58bc0532e2b5e0798459
Jon Balon <development@jonbalon.com>
2016-08-10 19:42:13 -0400

    Added boilerplate code

--- source/javascript/GeoMap.js
+++ source/javascript/GeoMap.js
@@ -1,1 +1,52 @@
-console.log('Hello world');
+// Use d3 library.
+var d3 = require('d3');
+
+// Canvas and graph settings.
+var canvas = {
+  dim: {
+    w: 500,
+    h: 500
+  },
+
+  graph: {
+  }
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// Callback to handle data retrieved from AJAX request
+////////////////////////////////////////////////////////////////////////////////
+var jsonSuccess = function(dataset) {
+
+  // Access the canvas and modify dimensions.
+  var svg = d3.select('#canvas')
+              .attr('width', canvas.dim.w)  // Canvas width
+              .attr('height', canvas.dim.h);// Canvas height
+
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// Callback to handle AJAX request errors
+////////////////////////////////////////////////////////////////////////////////
+var jsonError = function(error) {
+  console.log(error);
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// Load data
+////////////////////////////////////////////////////////////////////////////////
+d3.json('./datasets/meteorite-strike-data.json', function(error, result) {
+
+  // Error checking.
+  if (error) {
+
+    // Error function.
+    jsonError(error);
+
+  } else {
+
+    // If no error occurrs, run the jsonSuccess function.
+    jsonSuccess(result);
+
+  }
+
+});
